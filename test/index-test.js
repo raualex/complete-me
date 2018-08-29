bc2e3aa8ba07ad163807faad3123b3f179ca570f
Alexander Rau <alexanderrau86@yahoo.com>
2018-08-29 14:41:14 -0600

    Write insert method and start suggest method

--- test/index-test.js
+++ test/index-test.js
@@ -14,21 +14,42 @@ beforeEach(() => {
  })
 
  describe('insert', function() {
-  it ('should take a string as a parameter', function() {
+  it('should take a string as a parameter', function() {
     
     trie.insert('word');
 
     assert.isNotNull(trie.head)
   });
+
+  it.skip('should be able to add a node to the trie', function() {
+    
+    trie.insert('hello')
+
+    assert.equal(trie.head.data, 'h')
+  });
+
+  it.skip('should add children to the beginning node', function() {
+
+    trie.insert('bleistift')
+    let firstNode = trie.head.children
+   
+    assert.equal(firstNode[0].data, 'l')
+  })
+
  })
 
  describe('count', function() {
-  it ('should be able to count the words inserted in', function() {
+  it('should be able to count the words inserted in', function() {
     
     assert.equal(trie.wordCount, 0)
     trie.insert('word');
 
     assert.equal(trie.wordCount, 1)
+
+    trie.insert('wonder')
+
+    assert.equal(trie.wordCount, 2)
+    console.log(JSON.stringify(trie.root, null, 4))
   })
  })
  
